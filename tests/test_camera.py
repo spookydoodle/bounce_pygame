d96381bfe51e03f0322817394cff99bd23a6ab43
rwakulszowa <rwakulszowa1@gmail.com>
2018-09-08 09:59:15 +0200

    Fixed tests

--- tests/test_camera.py
+++ tests/test_camera.py
@@ -1,26 +1,23 @@
 from unittest import TestCase
 
-from skater.camera import Camera
+from silnik.rendering.point import Point
+from silnik.rendering.shape import rectangle
 
-from .mock_rect import MockRect, shifted
+from bounce.camera import Camera
 
 screen_width = 400
 screen_height = 300
 screen_center_x = screen_width / 2
 screen_center_y = screen_height / 2
 
-screen = MockRect(
-    0,
-    screen_height,
-    0,
-    screen_width)
+screen = rectangle(
+    Point(0, 0),
+    Point(screen_width, screen_height))
 
 # The player is in the center of the screen
-base_player = MockRect(
-    screen_center_y - 10,
-    screen_center_y + 10,
-    screen_center_x - 10,
-    screen_center_x + 10)
+base_player = rectangle(
+    Point(screen_center_x - 10, screen_center_y - 10),
+    Point(screen_center_x + 10, screen_center_y + 10))
 
 class TestAdjustX(TestCase):
     def test_does_nothing_if_player_in_the_center(self):
@@ -30,13 +27,13 @@ class TestAdjustX(TestCase):
 
     def test_increases_x_if_player_on_the_right(self):
         camera = Camera()
-        player = shifted(base_player, x_shift=screen_width)
+        player = base_player.shifted(x_shift=screen_width)
         camera.adjust_x(screen, player)
         self.assertGreater(camera.x, 0)
 
     def test_decreases_x_if_player_on_the_left(self):
         camera = Camera()
-        player = shifted(base_player, x_shift=-screen_width)
+        player = base_player.shifted(x_shift=-screen_width)
         camera.adjust_x(screen, player)
         self.assertLess(camera.x, 0)
 
@@ -48,12 +45,12 @@ class TestAdjustY(TestCase):
 
     def test_increases_y_if_player_on_the_bottom(self):
         camera = Camera()
-        player = shifted(base_player, y_shift=screen_height)
+        player = base_player.shifted(y_shift=screen_height)
         camera.adjust_y(screen, player)
         self.assertGreater(camera.y, 0)
 
     def test_decreases_y_if_player_on_the_top(self):
         camera = Camera()
-        player = shifted(base_player, y_shift=-screen_height)
+        player = base_player.shifted(y_shift=-screen_height)
         camera.adjust_y(screen, player)
         self.assertLess(camera.y, 0)
